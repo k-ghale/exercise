e93ad9010aff2a1d7c8f4d6834497ea00075de10
k-ghale <kabinghale888@gmail.com>
2026-03-04 10:45:09 -0500

    practice: recursion is fun

--- Sorting/1.check_if_sorted.py
+++ Sorting/1.check_if_sorted.py
@@ -1,18 +1,22 @@
 
-arr = [10,20,3,4,5,6]
-
 #using recursion
-def check_sort_helper(arr, n):
-    if (n == 0 or n ==1):
+def check_sorter_helper(arr, n):
+    #base case
+    if(n == 0 or n == 1):
         return True
-    
-    return (arr[n-1] >= arr[n-2] and check_sort_helper(arr, n-1))
+    #check
+    return (arr[n-1] >= arr[n-2] and check_sorter_helper(arr,n-1))
 
-def check_sort(arr):
+def check(arr):
     n = len(arr)
-    return check_sort_helper(arr, n)
+    return check_sorter_helper(arr, n)
 
+if __name__ == "__main__":
+    #test
+    unsorted_arr = [10,20,3,4,5,6]
+    sorted_arr = [10,20,30,40,50,60]
 
-
-print(check_sort(arr))
+    #print
+    print(check(unsorted_arr))
+    print(check(sorted_arr))
 
