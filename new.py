c04a6efdb3e858b833c416238f356edd338ab6be
k-ghale <kabinghale888@gmail.com>
2026-03-08 18:45:36 -0400

    add: find max

--- new.py
+++ new.py
@@ -8,3 +8,18 @@ print(math.log(16,2))
 foo("kabin");
 
 2 * 2 *2 *2
+
+
+def find_max(nums):
+
+    n = len(nums)
+    largest = 0
+    for i in range(n):
+        if(nums[i] > largest):
+            largest = nums[i]
+            print(largest)
+
+    return largest
+
+print(find_max([-1, -2, -3]))
+
