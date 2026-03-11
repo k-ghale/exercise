04e9c2e83cc274046ca5660fe030da796866f2c0
kabinghale <kabinghale888@gmail.com>
2026-03-11 13:41:14 -0400

    add: find last name

---
+++ find_last_name.py
@@ -0,0 +1,6 @@
+
+def find_last_name(names_dict, first_name):
+    try:
+        return names_dict[first_name]
+    except KeyError:
+        return None
--- get_avg_brand_followers.py
+++ get_avg_brand_followers.py
@@ -1,2 +1,9 @@
-def get_avg_brand_followers():
-    pass
+def get_avg_brand_followers(all_handles,brand_name):
+    total = 0
+    for handle in all_handles:
+        for brand in handle:
+            if brand_name in brand:
+                total += 0
+
+    avg = total / len(all_handles)
+    return avg
