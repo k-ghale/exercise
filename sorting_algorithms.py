b346ddfaa5f1a544e2d73d81c5d8183ba38d25a9
k-ghale <kabinghale888@gmail.com>
2026-03-13 15:52:02 -0400

    add: sorting_algorithms

---
+++ sorting_algorithms.py
@@ -0,0 +1,22 @@
+class Influencer:
+    def __init__(self, num_selfies, num_bio_links):
+        self.num_selfies = num_selfies
+        self.num_bio_links = num_bio_links
+
+    def __repr__(self):
+        return f"({self.num_selfies}, {self.num_bio_links})"
+
+
+# dont touch above this line
+
+
+def vanity(influencer):
+    (num_bio_links * 5) + num_selfies
+
+
+def vanity_sort(influencers):
+    key = False
+    sorted(influencers)
+    key = True
+
+    return(influencers)
