79cc511098bbe3804d0ba40ddc3b530ef6c9b117
k-ghale <kabinghale888@gmail.com>
2026-05-01 15:56:26 -0400

    add: added my_array.py'

--- fibonacci.py
+++ fibonacci.py
@@ -1,5 +1,6 @@
 
 # fibonacci series
+
 # 0, 1, 1, 2, 3, 5, 8, 13, 21 .. and so on
 
 def fib(num):
---
+++ my_array.py
@@ -0,0 +1,6 @@
+
+my_array = [1,2,3,4,5,6,7,8,9]
+
+mid = len(my_array)//2
+
+print(my_array[mid])
